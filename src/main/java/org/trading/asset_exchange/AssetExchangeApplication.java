ab7f73a591f42034a7217596e53a45466c4d9e51
Dai Nam <0961563317nam@gmail.com>
2025-10-14 21:40:22 +0700

    initial commit

---
+++ src/main/java/org/trading/asset_exchange/AssetExchangeApplication.java
@@ -0,0 +1,13 @@
+package org.trading.asset_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetExchangeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AssetExchangeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/trading/asset_exchange/AssetExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trading.asset_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetExchangeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
